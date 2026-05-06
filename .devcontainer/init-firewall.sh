a5cc6161cef6e43be741676f02f8043272277a08
ujiie.kazuaki <ujiie.kazuaki.59@gmail.com>
2026-05-06 00:52:29 -0700

    devcontainer firewall修正 + tmux対応

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -65,6 +65,7 @@ done < <(echo "$gh_ranges" | jq -r '(.web + .api + .git)[]' | aggregate -q)
 
 # Resolve and add other allowed domains
 for domain in \
+    "deb.debian.org" \
     "registry.npmjs.org" \
     "api.anthropic.com" \
     "sentry.io" \
